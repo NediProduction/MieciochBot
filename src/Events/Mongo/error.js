6db16bbaa797b2246a1f6976bf9ae470803bf526
Nedi <github@nedi.me>
2022-05-29 00:56:08 +0200

    clear command

--- src/Events/Mongo/error.js
+++ src/Events/Mongo/error.js
@@ -3,6 +3,6 @@ const chalk = require('chalk');
 module.exports = {
 	name: 'error',
 	async execute(error) {
-		console.log(chalk.hex('#96FB6D').bold(error));
+		console.log(chalk.red.bold(error));
 	},
 };
--- src/Events/interactionCreate.js
+++ src/Events/interactionCreate.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const chalk = require('chalk');
 
 module.exports = {
@@ -14,7 +15,24 @@ module.exports = {
 			await command.execute(interaction, client);
 		} catch (error) {
 			console.error(error);
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			const embed = new MessageEmbed()
+				.setColor('#2f3136')
+				.setDescription('<a:Cross_Wik:865613743907667969> **Wystąpił błąd podczas wykonywania tego polecenia!**');
+			await interaction.reply({ embeds: [embed], ephemeral: true });
 		}
 	},
 };
+
+/* const embed = new MessageEmbed()
+ *	.setColor('#2f3136')
+ *	.setTitle('Polecenie Interakcji')
+ *	.setFields(
+ *		{ name: 'Użykownik:', value: `${interaction.guild.members.cache.get(interaction.user.id)}`, inline: true },
+ *		{ name: '\u200B', value: '\u200B', inline: true },
+ *		{ name: 'ID Użytkownika:', value: `\`${interaction.user.id}\``, inline: true },
+ *		{ name: 'Kanał:', value: `${interaction.guild.channels.cache.get(interaction.channel.id)}`, inline: true },
+ *		{ name: '\u200B', value: '\u200B', inline: true },
+ *		{ name: 'Data:', value: `<t:${parseInt(interaction.createdTimestamp / 1000)}:f>`, inline: true },
+ *		{ name: 'Komenda:', value: `\`\`\`/${interaction.commandName}\`\`\`` },
+ *	);
+ */
