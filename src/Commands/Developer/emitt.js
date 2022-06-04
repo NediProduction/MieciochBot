adbecd2ba585ec7bf5c59e652e8f24d28f5eeb0d
Nedi <github@nedi.me>
2022-06-04 22:29:28 +0200

    emitt

---
+++ src/Commands/Developer/emitt.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('emitt')
+		.setDescription('Emiter zdarzeń.')
+		.addStringOption(option =>
+			option.setName('użytkownik')
+				.setDescription('Wydarzenia dla członków serwera.')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'guildMemberAdd', value: 'guildMemberAdd' },
+					{ name: 'guildMemberRemove', value: 'guildMemberRemove' },
+				)),
+	async execute(interaction, client) {
+		const choices = interaction.options.getString('użytkownik');
+
+		switch (choices) {
+			case 'guildMemberAdd': {
+				client.emit('guildMemberAdd', interaction.member);
+				interaction.reply({ content: 'Emitowano wydarzenie.', ephemeral: true });
+			}
+				break;
+			case 'guildMemberRemove': {
+				client.emit('guildMemberRemove', interaction.member);
+				interaction.reply({ content: 'Emitowano wydarzenie.', ephemeral: true });
+			}
+				break;
+		}
+	},
+};
